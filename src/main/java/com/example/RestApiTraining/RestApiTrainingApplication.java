3732272d7f32426dc378f10af55b1491ba21348c
nagamoto <koutaro.n.1056@gmail.com>
2022-09-16 00:38:21 +0900

    first commit

---
+++ src/main/java/com/example/RestApiTraining/RestApiTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestApiTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTrainingApplication.class, args);
+	}
+
+}
